43f149c2ec68fa33448dd339f1104bc2d30b6165
Ewerton Luis <ewerton.santana@ucsal.edu.br>
2020-04-09 18:35:01 -0300

    Adicionada regressão linear

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     private static final String[] FILES_NAMES = {"DispatchQueue.txt",
@@ -9,6 +12,7 @@ public class Main {
             "UserConfig.txt",
             "Utilities.txt"};
     private static final String SEPARADOR = ",";
+    private static List<RetornoDTO> retornoDTOList = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         FileWriter csvWriter = new FileWriter("new.csv");
@@ -23,27 +27,42 @@ public class Main {
         csvWriter.append("CLASSES_DEUS");
         csvWriter.append(SEPARADOR);
         csvWriter.append("METODOS_DEUS");
-
+        List<Integer> meses = new ArrayList<>();
         for (Integer i = 1; i <= 27; i++) {
-            RetornoDTO retorno = new RetornoDTO(0,0,0, 0, 0);
+            RetornoDTO retornoMes = new RetornoDTO(0,0,0, 0, 0);
             File file;
             for (String fileName : FILES_NAMES) {
                 file = new File(i.toString() + "/" + fileName);
-                retorno.sumWithAnother(FileAnalizer.analyzeFile(file));
+                retornoMes.sumWithAnother(FileAnalizer.analyzeFile(file));
             }
+            meses.add(i);
+            retornoDTOList.add(retornoMes);
             csvWriter.append("\n");
             csvWriter.append(i.toString());
             csvWriter.append(SEPARADOR);
-            csvWriter.append(retorno.getLoc().toString());
+            csvWriter.append(retornoMes.getLoc().toString());
             csvWriter.append(SEPARADOR);
-            csvWriter.append(retorno.getClasses().toString());
+            csvWriter.append(retornoMes.getClasses().toString());
             csvWriter.append(SEPARADOR);
-            csvWriter.append(retorno.getMethods().toString());
+            csvWriter.append(retornoMes.getMethods().toString());
             csvWriter.append(SEPARADOR);
-            csvWriter.append(retorno.getNumberOfGodClasses().toString());
+            csvWriter.append(retornoMes.getNumberOfGodClasses().toString());
             csvWriter.append(SEPARADOR);
-            csvWriter.append(retorno.getNumberOfGodMethods().toString());
+            csvWriter.append(retornoMes.getNumberOfGodMethods().toString());
         }
+        final Integer MES_28 = 28;
+        csvWriter.append("\n");
+        csvWriter.append(MES_28.toString());
+        csvWriter.append(SEPARADOR);
+        csvWriter.append(new RegrecaoLinear(meses,retornoDTOList.stream().map(RetornoDTO::getLoc).collect(Collectors.toList())).prever(MES_28).toString());
+        csvWriter.append(SEPARADOR);
+        csvWriter.append(new RegrecaoLinear(meses,retornoDTOList.stream().map(RetornoDTO::getClasses).collect(Collectors.toList())).prever(MES_28).toString());
+        csvWriter.append(SEPARADOR);
+        csvWriter.append(new RegrecaoLinear(meses,retornoDTOList.stream().map(RetornoDTO::getMethods).collect(Collectors.toList())).prever(MES_28).toString());
+        csvWriter.append(SEPARADOR);
+        csvWriter.append(new RegrecaoLinear(meses,retornoDTOList.stream().map(RetornoDTO::getNumberOfGodClasses).collect(Collectors.toList())).prever(MES_28).toString());
+        csvWriter.append(SEPARADOR);
+        csvWriter.append(new RegrecaoLinear(meses,retornoDTOList.stream().map(RetornoDTO::getNumberOfGodMethods).collect(Collectors.toList())).prever(MES_28).toString());
         csvWriter.flush();
         csvWriter.close();
     }
---
+++ src/RegressaoLinear.java
@@ -0,0 +1,45 @@
+import java.util.List;
+
+public class RegrecaoLinear {
+    private List<Integer> variaveisIndepen;
+    private List<Integer> variaveisDepen;
+    private Double coeficienteDependente;
+    private Double coeficienteIndependente;
+    private int listSize;
+
+    public RegrecaoLinear(List<Integer> variaveisIndepen, List<Integer> variaveisDepen) {
+        if (variaveisDepen.size() != variaveisIndepen.size()) {
+            throw new IllegalArgumentException("As duas listas devem, obrigatoramente, ter o mesmo tamanho");
+        }
+        this.listSize = variaveisDepen.size();
+        this.variaveisIndepen = variaveisIndepen;
+        this.variaveisDepen = variaveisDepen;
+        calcularCoeficientes();
+    }
+
+    private void calcularCoeficientes() {
+        Long somatorioDepen = 0L;
+        Long somatorioIndepen = 0L;
+        double indepenQuadrado = 0D;
+        long somatorioCoeficiente = 0L;
+
+        for (int i = 0; i < listSize; i++) {
+            Integer variavelIndepen = variaveisIndepen.get(i);
+            Integer variavelDepen = variaveisDepen.get(i);
+
+            somatorioIndepen += variavelIndepen;
+            somatorioDepen += variavelDepen;
+
+            somatorioCoeficiente += variavelIndepen * variavelDepen;
+
+            indepenQuadrado = indepenQuadrado + Math.pow(variavelIndepen.doubleValue(), 2);
+        }
+
+        coeficienteDependente = ((listSize * somatorioCoeficiente) - (somatorioDepen * somatorioIndepen)) / ((listSize * indepenQuadrado) - (Math.pow(somatorioIndepen.doubleValue(), 2)));
+        coeficienteIndependente = (somatorioDepen / listSize) - (coeficienteDependente * (somatorioIndepen / listSize));
+    }
+
+    public Long prever(Integer valor) {
+        return Math.round(coeficienteDependente * valor + coeficienteIndependente);
+    }
+}
